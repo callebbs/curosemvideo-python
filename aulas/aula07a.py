341bdf23182bbb424f4e5cd4e792cf997102baf7
Calleb Bezerra <calleb.bezerra@gmail.com>
2020-08-24 16:16:39 -0300

    Create aula07a.py

---
+++ aulas/aula07a.py
@@ -0,0 +1,34 @@
+'''
+Operadores Aritméticos
+
++ = soma
+- = subtração
+* = multiplicação
+** = potenciação
+/ = divisão completa
+// = divisão inteira
+% = resto da divisão
+
+-----------------------------------
+
+Ordem de Precedência
+
+1 - ( )
+2 - **
+3 - * / // %
+4 - + -
+
+-----------------------------------
+
+Raizes quadradas e cúbicas
+
+25 ** (1/2)
+25 ** (1/3)
+
+-----------------------------------
+
+'''
+
+nome = input("Qual o seu nome? ")
+
+print(f"Prazer {}, seja bem vindo!")
