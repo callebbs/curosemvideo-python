9ee191bca393d06e40a79c7bf48dd7fbdbafb958
Calleb Bezerra <calleb.bezerra@gmail.com>
2020-08-24 15:19:32 -0300

    melhorei a questão com o format

--- exercicios/exercicio003.py
+++ exercicios/exercicio003.py
@@ -1,9 +1,12 @@
 '''
 Crie um script em Python que leia dois números e tente mostrar a soma entre eles.
+Desafio extra: colocar os números que foram digitados na mensagem final.
 '''
 numero1 = int(input('Digite um número: '))
 numero2 = int(input('Digite outro número: '))
 
 soma = (numero1 + numero2)
 
-print("A soma dos números digitados é:", soma)
+#print("A soma dos números", numero1, "e", numero2, "é:", soma)
+
+print("A soma de {} e {} é igual a {}.".format(numero1, numero2, soma))
---
+++ exercicios/exercicio004.py
@@ -0,0 +1,4 @@
+'''
+Faça um programa que leia algo pelo teclado e mostre na tela seu tipo primitivo e todas as informações possíveis sobre ele.
+'''
+
