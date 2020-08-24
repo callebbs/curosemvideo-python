bb624114e2764ea2b1e807436687879b1ab50d51
Calleb Bezerra <calleb.bezerra@gmail.com>
2020-08-24 16:40:15 -0300

    estudos da aula 07

--- aulas/aula07a.py
+++ aulas/aula07a.py
@@ -26,9 +26,33 @@ Raizes quadradas e cúbicas
 25 ** (1/3)
 
 -----------------------------------
+Alinhamento dentro da formatação:
+
+#print(f'Prazer {nome:>20}, seja bem vindo!')
+
+> alinhado a direita
+< alinhado a esquerda
+^ centralizado
+= antes do sinal para adicionar ao texto
+
+#print(f'Prazer {nome:=^20}, seja bem vindo!')
 
 '''
+#primeiro código
+#nome = input("Qual o seu nome? ")
+#print(f'Prazer {nome:=^20}, seja bem vindo!')
 
-nome = input("Qual o seu nome? ")
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite outro número: "))
 
-print(f"Prazer {}, seja bem vindo!")
+soma = numero1 + numero2
+multi = numero1 * numero2
+divi = numero1 / numero2
+divint = numero1 // numero2
+exp = numero1 ** numero2
+
+print(f"A soma é: {soma}, o produto é: {multi} e a divisão é: {divi:.3f}")
+print(f"A divisão inteira é: {divint} e a potência é: {exp}")
+
+#formatar o resultado float: :.3f
+#.3 diminuindo para 3 casas decimais e f de float
