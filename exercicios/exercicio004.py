d90ec27d3b1a4707cc8bee023428c75e46906ebb
Calleb Bezerra <calleb.bezerra@gmail.com>
2020-08-24 15:34:08 -0300

    adicionado exercício 04

--- exercicios/exercicio004.py
+++ exercicios/exercicio004.py
@@ -2,3 +2,21 @@
 Faça um programa que leia algo pelo teclado e mostre na tela seu tipo primitivo e todas as informações possíveis sobre ele.
 '''
 
+texto = input("Digite algo: ")
+separador = 50 * "-"
+print("O tipo primitivo do que foi digitado é: ", type(texto))
+print(separador)
+print("O O que foi digitado é um alfanumérico? ", texto.isalnum())
+print(separador)
+print("O que foi digitado pertence ao alfabético? ", texto.isalpha())
+print(separador)
+print("O que foi digitado pertence ao numérico? ", texto.isnumeric())
+print(separador)
+print("O que foi digitado é apenas minúsculo? ", texto.islower())
+print(separador)
+print("O que foi digitado é apenas maiúsculo? ", texto.isupper())
+print(separador)
+print("O que foi digitado está capitalizada? ", texto.istitle())
+print(separador)
+print("O que foi digitado é apenas um espaço? ", texto.isspace())
+print(separador)
