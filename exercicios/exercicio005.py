e6181842ac5f7f4230fd92c68e8690232f0dea16
Calleb Bezerra <calleb.bezerra@gmail.com>
2020-11-15 14:34:04 -0300

    Update exercicio005.py

--- exercicios/exercicio005.py
+++ exercicios/exercicio005.py
@@ -5,3 +5,5 @@ Faça um programa que leia um número inteiro e mostra na tela seu sucessor e se
 numero = (int(input("Digite um número inteiro: ")))
 print(f"Você digitou o número {numero}. Seu anterior é:", numero - 1, "e o sucessor:", numero + 1)
 
+#outra forma de mostrar utilizando .format no final
+#print("Você digitou o número {}. Seu anterior é: {} e seu sucessor é {}".format(numero, numero - 1, numero + 1))
